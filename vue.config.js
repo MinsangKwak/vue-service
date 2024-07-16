4e004dc6fec327eb336169e1737beb3c38de2475
minsangKwak <kmsdevwork@gmail.com>
2024-07-17 00:21:40 +0900

    update branch

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath:
-		process.env.NODE_ENV === "production"
-			? "/<vue-service>/" // 여기서 <repository-name>을 GitHub 저장소 이름으로 변경합니다.
-			: "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/vue-service/" : "/",
 });
