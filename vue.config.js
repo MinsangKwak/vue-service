bda1d509bf475889656b9d50ce3f00cdd6cf342b
minsangKwak <kmsdevwork@gmail.com>
2024-07-16 23:17:37 +0900

    Add publicPath to vue.config.js for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	publicPath:
+		process.env.NODE_ENV === "production"
+			? "/<repository-name>/" // 여기서 <repository-name>을 GitHub 저장소 이름으로 변경합니다.
+			: "/",
+});
