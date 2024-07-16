7093f41b1c3006307426cd3adb93e38f0e77c7f9
minsangKwak <kmsdevwork@gmail.com>
2024-07-16 23:24:37 +0900

    Add publicPath to vue.config.js for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
 	transpileDependencies: true,
 	publicPath:
 		process.env.NODE_ENV === "production"
-			? "/<repository-name>/" // 여기서 <repository-name>을 GitHub 저장소 이름으로 변경합니다.
+			? "/<sample-service>/" // 여기서 <repository-name>을 GitHub 저장소 이름으로 변경합니다.
 			: "/",
 });
